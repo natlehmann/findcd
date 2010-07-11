c5762adb79ad75280970aad1ab8cbfdcf0b5332e
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-11 19:35:29 +0000

    removed spring managed transactions

--- findcd/trunk/test/ar/com/natlehmann/cdcatalogue/dao/jpa/CategoryDaoTest.java
+++ findcd/trunk/test/ar/com/natlehmann/cdcatalogue/dao/jpa/CategoryDaoTest.java
@@ -1,12 +1,16 @@
 package ar.com.natlehmann.cdcatalogue.dao.jpa;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Test;
 
 import ar.com.natlehmann.cdcatalogue.business.model.Category;
-import ar.com.natlehmann.cdcatalogue.dao.CategoryDao;
 import ar.com.natlehmann.cdcatalogue.dao.DaoBaseTest;
 import ar.com.natlehmann.cdcatalogue.dao.DaoException;
 
@@ -16,41 +20,35 @@ import ar.com.natlehmann.cdcatalogue.dao.DaoException;
  *
  */
 public class CategoryDaoTest extends DaoBaseTest {
-	
-	private CategoryDao categoryDao;
-	
-	public void setCategoryDao(CategoryDao categoryDao) {
-		this.categoryDao = categoryDao;
-	}
 
 	
 	@Test
 	public void testFindByName() throws DaoException, SQLException {
-		assertNotNull(this.categoryDao.findCategory("CategoriaPrueba"));
-		assertNull(this.categoryDao.findCategory("ategoriaPrueba"));
+		assertNotNull(this.getCategoryDao().findCategory("CategoriaPrueba"));
+		assertNull(this.getCategoryDao().findCategory("ategoriaPrueba"));
 	}
 	
 	@Test
 	public void testPersistence() throws DaoException, SQLException {
 		Category t = new Category("tirar");
-		int initialCount = categoryDao.getCategories().size();
+		int initialCount = this.getCategoryDao().getCategories().size();
 		
-		Category o = this.categoryDao.createCategory(t);
+		Category o = this.getCategoryDao().createCategory(t);
 
 		assertNotNull(o.getCategoryId());
-		assertNotNull(this.categoryDao.findCategory("tirar"));
-		assertTrue(categoryDao.getCategories().size() == initialCount + 1);
+		assertNotNull(this.getCategoryDao().findCategory("tirar"));
+		assertTrue(this.getCategoryDao().getCategories().size() == initialCount + 1);
 
-		this.categoryDao.deleteCategory(t);
+		this.getCategoryDao().deleteCategory(t);
 
-		assertNull(this.categoryDao.findCategory("tirar"));
-		assertTrue(categoryDao.getCategories().size() == initialCount);
+		assertNull(this.getCategoryDao().findCategory("tirar"));
+		assertTrue(this.getCategoryDao().getCategories().size() == initialCount);
 
 	}
 	
 	@Test
 	public void testGetCategoryVolumes() throws DaoException {
-		List<Category> categories = this.categoryDao.getCategoryVolumes();
+		List<Category> categories = this.getCategoryDao().getCategoryVolumes();
 		assertNotNull(categories);
 		assertFalse(categories.isEmpty());
 		assertTrue(categories.size() >= 2);
@@ -76,7 +74,7 @@ public class CategoryDaoTest extends DaoBaseTest {
 	@Test
 	public void testGetCategories() throws DaoException {
 		
-		List<Category> categories = this.categoryDao.getCategories();
+		List<Category> categories = this.getCategoryDao().getCategories();
 		assertNotNull(categories);
 		assertFalse(categories.isEmpty());
 		assertTrue(categories.size() >= 2);
