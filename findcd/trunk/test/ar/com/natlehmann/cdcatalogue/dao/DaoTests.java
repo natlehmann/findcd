1ecf4ae26cd41b206a34983a9517738f28106e36
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-11 19:34:28 +0000

    removed spring managed transactions

--- findcd/trunk/test/ar/com/natlehmann/cdcatalogue/dao/DaoTests.java
+++ findcd/trunk/test/ar/com/natlehmann/cdcatalogue/dao/DaoTests.java
@@ -1,30 +1,29 @@
 package ar.com.natlehmann.cdcatalogue.dao;
 
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 import ar.com.natlehmann.cdcatalogue.dao.jpa.CategoryDaoTest;
 import ar.com.natlehmann.cdcatalogue.dao.jpa.ResourceCategoryDaoTest;
 import ar.com.natlehmann.cdcatalogue.dao.jpa.ResourceDaoTest;
 import ar.com.natlehmann.cdcatalogue.dao.jpa.VolumeDaoTest;
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
 /**
  * To run these tests you must first run the script sql/InsertTestData.sql on the DB
  * @author natalia
  *
  */
+@RunWith(Suite.class)
+@SuiteClasses( { PageTest.class, CategoryDaoTest.class, 
+	ResourceDaoTest.class, VolumeDaoTest.class, ResourceCategoryDaoTest.class })
 public class DaoTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite(
-				"Test for ar.com.natlehmann.cdcatalogue.dao");
-
-		suite.addTestSuite(PageTest.class);
-		suite.addTestSuite(CategoryDaoTest.class);
-		suite.addTestSuite(ResourceDaoTest.class);
-		suite.addTestSuite(VolumeDaoTest.class);
-		suite.addTestSuite(ResourceCategoryDaoTest.class);
-
-		return suite;
+		return new JUnit4TestAdapter(DaoTests.class);
 	}
 
 }
