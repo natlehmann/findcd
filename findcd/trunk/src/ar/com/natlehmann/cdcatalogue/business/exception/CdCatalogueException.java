e5334651395894bc51c91b7e98559346e14ac7c6
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-16 01:59:10 +0000

    removed TODOs

--- findcd/trunk/src/ar/com/natlehmann/cdcatalogue/business/exception/CdCatalogueException.java
+++ findcd/trunk/src/ar/com/natlehmann/cdcatalogue/business/exception/CdCatalogueException.java
@@ -5,22 +5,18 @@ public class CdCatalogueException extends Exception {
 	private static final long serialVersionUID = -370924045345758480L;
 
 	public CdCatalogueException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public CdCatalogueException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CdCatalogueException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CdCatalogueException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
