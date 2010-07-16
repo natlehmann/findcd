21a3d243a98901a16860c2d1dde9f6783e143ba1
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-16 01:57:48 +0000

    new

---
+++ findcd/trunk/src/ar/com/natlehmann/cdcatalogue/business/CdCatalogueModelObserver.java
@@ -0,0 +1,19 @@
+package ar.com.natlehmann.cdcatalogue.business;
+
+import java.util.List;
+
+import ar.com.natlehmann.cdcatalogue.business.model.Category;
+import ar.com.natlehmann.cdcatalogue.business.model.Volume;
+
+public interface CdCatalogueModelObserver {
+
+	void categoryCreated(Category newCategory);
+	void categoryUpdated(Category category);
+
+	void volumeCreated(Volume newVolume);
+	void volumesUpdated(List<Volume> volumes);
+	void volumeDeleted(Volume volume);
+	void volumeUpdated(Volume volume);
+
+
+}
