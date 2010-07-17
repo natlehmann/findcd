64068747463fc1cca12d7e708596213aafcb6d80
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-17 13:42:34 +0000

    removed TODOs

--- findcd/trunk/src/ar/com/natlehmann/cdcatalogue/business/exception/InvalidNameException.java
+++ findcd/trunk/src/ar/com/natlehmann/cdcatalogue/business/exception/InvalidNameException.java
@@ -5,22 +5,18 @@ public class InvalidNameException extends CdCatalogueException {
 	private static final long serialVersionUID = 8810986014717117987L;
 
 	public InvalidNameException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidNameException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidNameException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidNameException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
