6a642b1aa0aae9433f8454ceb434921b8a7e3c4f
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-17 13:44:06 +0000

    removed TODOs

--- findcd/trunk/src/ar/com/natlehmann/cdcatalogue/dao/NonUniqueResultExcetion.java
+++ findcd/trunk/src/ar/com/natlehmann/cdcatalogue/dao/NonUniqueResultExcetion.java
@@ -5,22 +5,18 @@ public class NonUniqueResultExcetion extends DaoException {
 	private static final long serialVersionUID = -703313374914454511L;
 
 	public NonUniqueResultExcetion() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public NonUniqueResultExcetion(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NonUniqueResultExcetion(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NonUniqueResultExcetion(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 }
