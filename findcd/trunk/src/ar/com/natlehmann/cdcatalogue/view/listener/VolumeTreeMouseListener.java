38aa4efc08ef1b48252c5fb3f4b4121cedfb023b
natlehmann <natlehmann@967bba3d-a9c1-c4f5-e4dc-571b2997ae33>
2010-07-17 13:49:55 +0000

    removed TODOs

--- findcd/trunk/src/ar/com/natlehmann/cdcatalogue/view/listener/VolumeTreeMouseListener.java
+++ findcd/trunk/src/ar/com/natlehmann/cdcatalogue/view/listener/VolumeTreeMouseListener.java
@@ -47,25 +47,21 @@ public class VolumeTreeMouseListener implements MouseListener {
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 
 	}
 
